3f1324aa956e28c3196233229e4cd03c9492a176
rbrgasilva <ritabga@gmail.com>
2025-02-18 09:39:06 -0300

    Adiciona classe Calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,9 +4,13 @@ public class Calculadora {
     public static int soma(int a, int b){
         return a + b;
     }
+    public static int subtracao(int a, int b){
+        return a - b;
+    }
     public static void main(String[] args) {
 
         System.out.println("Resultado: " + soma(9,1));
+        System.out.println("Resultado: " + subtracao(9,1));
 
 
     }
